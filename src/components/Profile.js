1f5c7c29c8a3be25c230a434bb9e81379160c430
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-05-13 19:14:01 -0400

    Starting to add friends and friends request menus in profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,12 +6,17 @@ import PostList from './PostList';
 const Profile = () => {
     const [user, setUser] = useState(null);
     const [posts, setPosts] = useState([]);
+    const [content, setContent] = useState(0)
     const {id} = useParams();
 
     const getData = async () => {
         const {userDetail, userPosts} = await getUserDetail(id);
         setUser(userDetail);
         setPosts(userPosts);
+    }
+
+    const changeContent = (value) => {
+        setContent(value)
     }
 
     useEffect(() => {
@@ -22,6 +27,12 @@ const Profile = () => {
         return (
             <div>
                 <p>{user.firstName}</p>
+                {content}
+                <div>
+                    <button onClick={e => changeContent(0)}>Post</button>
+                    <button onClick={e => changeContent(1)}>Friends</button>
+                    <button onClick={e => changeContent(2)}>Friend Requests</button>
+                </div>
                 <PostList posts={posts}/>
             </div>
         )
