56d177d166c746951822e5fda1f25bd47594ba9a
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-05-20 23:34:41 -0400

    Protected remove friend

--- src/components/DeleteFriend.js
+++ src/components/DeleteFriend.js
@@ -2,16 +2,23 @@ import {removeFriend} from '../functions/userCalls';
 
 const DeleteFriend = (props) => {
     const {user} = props;
+    console.log(user);
     const sender = JSON.parse(localStorage.getItem('userSession')).user._id;
     const friend = user._id;
 
     const submitData = async () => {
         await removeFriend({sender, friend})
     }
+
+    const deleteButton = (user.friendList.includes(sender)) ? (
+        <button onClick={submitData}>Delete Friend</button>
+    ): (
+        null
+    );
     
     return (
         <div>
-            <button onClick={submitData}>Delete Friend</button>
+            {deleteButton}
         </div>
     )
 }
