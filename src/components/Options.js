8a1d4777c875808ed35e298b43a0243c63e4098d
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-05-24 15:56:23 -0400

    Refactored edit button

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,12 @@
 import verifySession from '../functions/verifySession';
-import {Link} from 'react-router-dom';
 import { useState } from 'react';
+import {useHistory } from 'react-router';
 
 const Options = (props) => {
     const {element, deleteFunction, likeFunction, editLink, initialLikeStatus, likes} = props;
     const [like, setLike] = useState(initialLikeStatus);
     const [likeCounter, setLikeCounter] = useState(likes.length);
+    const history = useHistory();
     
     const deleteData = async (e) => {
         e.preventDefault();
@@ -25,18 +26,21 @@ const Options = (props) => {
         }
     }
 
+    const editElement = (e) => {
+        e.preventDefault();
+        history.push(editLink);
+    }
+
     const userId = (verifySession()) ? JSON.parse(localStorage.getItem('userSession')).user._id : null;
     const deleteButton = (element.user._id === userId) ? <button onClick={deleteData}>Delete</button>: null;
-    const editButton = (element.user._id === userId) ? <button>Edit</button>: null;
+    const editButton = (element.user._id === userId) ? <button onClick={editElement}>Edit</button>: null;
     const likeButton = <button onClick={submitLike} className={like}>{likeCounter} {like}</button>
 
     return (
         <div className={'options'}>
             {likeButton}
             {deleteButton}
-                <Link to={editLink}>
-                    {editButton}
-                </Link>
+            {editButton}
         </div>
     )
 }
