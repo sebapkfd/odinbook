bf6abe5d98962d25f94b85b78c6764acbd8ad457
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-05-23 23:43:27 -0400

    Added style to comment items and refactored Post text margins

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -20,8 +20,10 @@ const CommentItem = (props) => {
 
     return (
         <div className={'comment-item'}>
-            <p>{comment.text}</p>
-            <p>{comment.user.firstName} {comment.user.lastName}</p>
+            <p className={'comment-item__username'}>{comment.user.firstName} {comment.user.lastName}</p>
+            <div className={'comment-item__text'}>
+                <p>{comment.text}</p>
+            </div>
             <Options
                 element={comment}
                 deleteFunction={deleteData}
