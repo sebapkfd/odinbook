f060cc87e9b7c43b105b014be86221dbe44be60f
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-05-25 19:11:11 -0400

    Refactored logic after send/cancel request

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,26 +1,30 @@
+import { useState } from 'react';
 import {sendFriendRequest, cancelFriendRequest} from '../functions/userCalls';
 
 const AddFriend = (props) => {
     const {user} = props;
     const sender = JSON.parse(localStorage.getItem('userSession')).user._id;
     const receiver = user._id;
+    const [request, setRequest] = useState('Add Friend');
+    const content = {
+        'Add Friend': <button onClick={() => sendRequest()}>Add Friend</button>,
+        'Cancel Request': <button onClick={()=> cancelRequest()}>Cancel Request</button>
+    };
 
     const sendRequest = async () => {
         await sendFriendRequest({sender, receiver});
+        setRequest('Cancel Request');
     }
 
     const cancelRequest = async () => {
         await cancelFriendRequest({sender, receiver});
+        setRequest('Add Friend');
     }
 
-    const addFriendButton = (user.friendRequests.includes(sender)) ? (
-        <button onClick={cancelRequest}>Cancel Request</button>
-    ) : (
-        <button onClick={sendRequest}>Add Friend</button>
-    );
-
     return (
-        <div className={'add-friend'}>{addFriendButton}</div>
+        <div className={'add-friend'}>
+            {content[request]}
+        </div>
     )
 
 }
